0ac156b1f13f0de40ad1f4a3028d368a251a7457
light-oj <46436281+light-oj@users.noreply.github.com>
2019-07-06 09:38:50 +0100

    Add files via upload

---
+++ Demo/scripts/demo.js
@@ -0,0 +1,20 @@
+alert("this calculates stuffs")
+document.write("Hello World!")
+
+// this is a comment line
+
+/* this
+is another 
+comment 
+example */
+
+var x = 1;
+var y = 20;
+
+var total = x + y;
+
+document.write(total)
+
+var x = 6
+
+document.write(x)
